a56a505f138c4deac5d9a17f2a154bcb04edf8df
kangjisoo <kangjs3779@gmail.com>
2023-09-01 17:13:33 +0900

    first commit

---
+++ src/main/java/programmers/test181933.java
@@ -0,0 +1,31 @@
+package programmers;
+
+public class test181933 {
+    public static void main(String[] args) {
+//        int a = -4;
+//        int b = 7;
+//        boolean flag = true;
+//
+//        int result = 0;
+//        if(flag) {
+//            result = a + b;
+//        } else {
+//            result = a - b;
+//        }
+//
+//        String ac = "1";
+//        Integer i = Integer.parseInt(ac);
+//        System.out.println(i);
+
+
+//        String answer = "*";
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(answer);
+//            answer += "*";
+//        }
+
+        for(int i = 0; i< 10; i++) {
+            System.out.println();
+        }
+    }
+}
