f5b3b0d6f000a51abf100fe105fddc5b92e511bd
kangjs3779 <kangjs3779@gmail.com>
2024-02-10 15:57:23 +0900

    test

--- src/main/java/programmers/test1844.java
+++ src/main/java/programmers/test1844.java
@@ -1,7 +1,25 @@
 package programmers;
 
 public class test1844 {
+
+
+    static int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+    static boolean[] visited = new boolean[maps.length];
+
     public static void main(String[] args) {
+        dfs(0);
 
     }
+
+    static void dfs(int nodeIndex) {
+        visited[nodeIndex] = true;
+        System.out.println("방문 : " + nodeIndex );
+
+        for(int node : maps[nodeIndex]) {
+            if(node == 1) {
+                dfs(node + 1);
+            }
+        }
+    }
+
 }
