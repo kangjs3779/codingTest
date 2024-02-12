6658da3c1cb64cd3bcaa8c17fc8b48ca603eb806
강지수 <123609533+kangjs3779@users.noreply.github.com>
2024-02-12 19:56:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.65 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,38 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] maps) {
-        // 갈 수 있는 방향 
-    	int[][] d = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         
-    	// 답 기본값 
+        int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        
         int answer = -1;
         
         Queue<int[]> queue = new LinkedList<>();
-        // 방문 했음을 표시 
+        
         maps[0][0] = 2;
-        // 현재 위치와 거리 
-        queue.add(new int[]{0, 0, 1});
-
-        while(!queue.isEmpty()) {
-            int[] coord = queue.poll();
+        queue.add(new int[]{0,0,1});
+        
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
             
-            // coord[0] = 세로 길이, coord[1] = 가로 길이 
-            // (세로, 가로)가 되면 목적지 도착한 것이므로 return 
-            if(coord[0] == maps.length - 1 && coord[1] == maps[0].length - 1) {
-                answer = coord[2];
+            if(current[0] == maps.length - 1 && current[1] == maps[0].length - 1) {
+                answer = current[2];
                 break;
             }
             
-            // 목적지 도착하지 않았으면 반복문 실행 
-            for(int i = 0; i < d.length; i++) {
-                int x = coord[0] + d[i][0], y = coord[1] + d[i][1];
-                if((0 <= x && x < maps.length) && (0 <= y && y < maps[0].length) && maps[x][y] == 1) {
-                	// 1일때만 움직일 수 있음 
-                	
+            for(int i = 0; i < direction.length; i++) {
+                int x = current[0] + direction[i][0];
+                int y = current[1] + direction[i][1];
+                
+                if(0 <= x && x < maps.length && 0 <= y && y < maps[0].length && maps[x][y] == 1) {
                     maps[x][y] = 2;
-                    // 방문 했음을 표시함 -> 2로 변경 
-                    queue.add(new int[]{x, y, coord[2] + 1});
-                    // 현재의 위치와, 거리를 더해줌 
+                    queue.add(new int[] {x, y, current[2] + 1});
+                    
                 }
             }
+            
         }
-
+        
         return answer;
     }
 }
